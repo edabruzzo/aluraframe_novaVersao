4fe1a024318f541baa0ee6bf22f5c959a230dab7
edabruzzo <emmanuel.oliveira3@gmail.com>
2019-01-23 20:00:01 -0200

    Começando encapsulamento dos campos da classe Negociacao

--- client/scripts/model/Negociacao.js
+++ client/scripts/model/Negociacao.js
@@ -4,28 +4,32 @@ class Negociacao{
     
      constructor(data, quantidade, valor) {
         
-      this.data = data;
-      this.quantidade = quantidade;
-      this.valor = valor;
+      this._data = data;
+      this._quantidade = quantidade;
+      this._valor = valor;
+
+      //evita que os campos sejam alterados após criação, congelando o objeto
+      //primeira tentativa de encapsulamento
+      Object.freeze(this);
       
         
     }
 
     
-        obtemVolume(){
-            return this.quantidade * this.valor;
+        get volume(){
+            return this._quantidade * this._valor;
         }
 
     
-    getData(){
-        return this.data;
+    get data(){
+        return this._data;
     }
         
-    getQuantidade(){
-        return this.quantidade;
+    get quantidade(){
+        return this._quantidade;
     }
 
-    getValor(){
-        return this.valor;
+    get valor(){
+        return this._valor;
     }
 }
